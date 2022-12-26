32ad00d7186db397b3459f908e0b0315e9a92eb9
Amir Yakubov <yakobub@gmail.com>
2022-12-26 09:28:04 +0200

    add more styling

--- cmps/book-preview.jsx
+++ cmps/book-preview.jsx
@@ -3,7 +3,7 @@ export function BookPreview({ book }) {
     return <article className="book-preview">
         <div className="text-wrap">
             <h2 className="book-card-title">{book.title}</h2>
-            <h3>Price: {book.listPrice.currencyCode} {book.listPrice.amount}</h3>
+            <h3 className="price-card-title">{book.listPrice.currencyCode} {book.listPrice.amount}</h3>
         </div>
         <img src={book.thumbnail} />
     </article>
